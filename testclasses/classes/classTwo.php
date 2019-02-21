c717e4d3f8e206f56db86899b37ce29f337c9d10
prod3v3loper <16712059+prod3v3loper@users.noreply.github.com>
2019-02-21 20:45:44 +0100

    Update classTwo.php

--- testclasses/classes/classTwo.php
+++ testclasses/classes/classTwo.php
@@ -5,8 +5,8 @@ namespace testclasses\classes;
 class class_two {
 
     public function method_from_class_two() {
-        
-       echo '<p>I am Class "<b>' . __CLASS__ . '</b> have a method <b>' . __METHOD__ . '</b></p>';
+
+       echo "<p>I am Class <b>", __CLASS__, "</b> have a method <b>", __METHOD__, "</b></p>";
     }
 
 }
